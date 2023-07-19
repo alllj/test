93b0537c10639482b0322c96819dde33b9847b94
hlm <179328112@qq.com>
2023-07-19 15:45:00 +0800

    test

--- src/test.java
+++ src/test.java
@@ -1,5 +1,11 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello dev!");
+        System.out.println("hello world");
     }
 }
+
+
