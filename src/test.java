7ae543b490f08f6633ff65c430a99283dce30ec0
hlm <179328112@qq.com>
2023-07-20 10:06:51 +0800

    test

--- src/test.java
+++ src/test.java
@@ -1,5 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello dev hlm lalala!");
+        System.out.println("Hello dev hlm hlm new!");
     }
 }
